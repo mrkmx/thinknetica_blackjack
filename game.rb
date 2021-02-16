2e1dd6981272d23d687c57adf961c845c5c97461
mrkmx <alex.a.maximow@gmail.com>
2021-02-16 19:58:06 +0400

    basic game

---
+++ game.rb
@@ -0,0 +1,29 @@
+require_relative "player"
+require_relative "bank"
+
+class Game
+  BASE_BET = 10
+
+  def initialize
+    # puts "Введите своё имя"
+    # player_name = gets.chomp
+    player_name = "mrkmx"
+
+    @player = Player.new player_name
+    @dealer = Player.new "Дилер"
+    @bank = Bank.new
+
+    start_game
+  end
+
+  def start_game
+    place_bets
+  end
+
+  def place_bets
+    @player.money=(@player.money - BASE_BET)
+    @dealer.money=(@dealer.money - BASE_BET)
+    
+    @bank.money=(@bank.money + (BASE_BET * 2))
+  end
+end
