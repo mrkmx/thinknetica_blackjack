488bc3a7ffe3eaf2cabdede073455070387d1cd1
mrkmx <alex.a.maximow@gmail.com>
2021-02-22 23:46:31 +0400

    rubocop

--- game.rb
+++ game.rb
@@ -1,6 +1,6 @@
-require_relative "player"
-require_relative "bank"
-require_relative "deck"
+require_relative 'player'
+require_relative 'bank'
+require_relative 'deck'
 
 class Game
   BASE_BET = 10
@@ -11,10 +11,10 @@ class Game
   def initialize
     # puts "Введите своё имя"
     # player_name = gets.chomp
-    player_name = "mrkmx"
+    player_name = 'mrkmx'
 
     @player = Player.new player_name
-    @dealer = Player.new "Дилер"
+    @dealer = Player.new 'Дилер'
     @bank = Bank.new
   end
 
@@ -27,17 +27,17 @@ class Game
 
   def continue
     over?
-    puts "Продолжить? (y/n)"
+    puts 'Продолжить? (y/n)'
     input = gets.chomp
-    if input == "y"
-      [@player, @dealer].each {|user| user.remove_cards}
-      [@player, @dealer].each {|user| user.remove_score}
+    if input == 'y'
+      [@player, @dealer].each { |user| user.remove_cards }
+      [@player, @dealer].each { |user| user.remove_score }
       start
-    elsif input == "n"
-      puts "Завершение игры"
+    elsif input == 'n'
+      puts 'Завершение игры'
       exit
     else
-      puts "Неизвестная команда"
+      puts 'Неизвестная команда'
       continue
     end
   end
@@ -52,10 +52,10 @@ class Game
   end
 
   def place_bets
-    @player.money=(@player.money - BASE_BET)
-    @dealer.money=(@dealer.money - BASE_BET)
-    
-    @bank.money=(@bank.money + (BASE_BET * 2))
+    @player.money = (@player.money - BASE_BET)
+    @dealer.money = (@dealer.money - BASE_BET)
+
+    @bank.money = (@bank.money + (BASE_BET * 2))
   end
 
   def deal_cards
@@ -69,26 +69,26 @@ class Game
   end
 
   def status
-    puts "========================="
+    puts '========================='
     puts "#{@player.name}: #{@player.card_names}"
     puts "Очки: #{@player.score}, Деньги: #{@player.money}"
-    puts "========================="
+    puts '========================='
     puts "#{@dealer.name}: #{@dealer.card_hidden_names}"
     puts "Очки: #{@dealer.score}, Деньги: #{@dealer.money}"
-    puts "========================="
+    puts '========================='
   end
 
   def dealer_actions
     if @dealer.score >= DEALER_THRESHOLD
-      puts "========================="
-      puts "Дилер пропускает ход"
-      puts "========================="
+      puts '========================='
+      puts 'Дилер пропускает ход'
+      puts '========================='
       winner if @dealer.score >= GAME_GOAL
       output
     else
-      puts "========================="
-      puts "Дилер берет карту"
-      puts "========================="
+      puts '========================='
+      puts 'Дилер берет карту'
+      puts '========================='
       @dealer.take_card @deck
       winner if max_cards? @dealer
       output
@@ -107,7 +107,7 @@ class Game
       puts "#{@dealer.name}: #{@dealer.score}, #{@dealer.card_names}"
       reward @player
     elsif @player.score == @dealer.score
-      puts "Ничья"
+      puts 'Ничья'
       puts "#{@player.name}: #{@player.score}, #{@player.card_names}"
       puts "#{@dealer.name}: #{@dealer.score}, #{@dealer.card_names}"
       reward @player, @dealer
@@ -115,48 +115,48 @@ class Game
     continue
   end
 
-  def reward *user
+  def reward(*user)
     if user.length > 1 # Ничья
       user.each do |u|
-        u.money=(u.money + BASE_BET)
-        @bank.money= 0
+        u.money = (u.money + BASE_BET)
+        @bank.money = 0
       end
     else # 1 победитель
-      user[0].money=(user[0].money + @bank.money)
-      @bank.money= 0
+      user[0].money = (user[0].money + @bank.money)
+      @bank.money = 0
     end
   end
 
   def user_actions
-    unless max_cards? @player
-      puts "1 - пропустить ход, 2 - показать карты, 3 - взять карту"
+    if max_cards? @player
+      puts '1 - пропустить ход, 2 - показать карты'
     else
-      puts "1 - пропустить ход, 2 - показать карты"
+      puts '1 - пропустить ход, 2 - показать карты, 3 - взять карту'
     end
-    
+
     input = gets.chomp.to_i
     case input
-      when 1
-        dealer_actions
-      when 2
-        winner
-      when 3
-        begin
-          raise "Взято максимальное количество карт" if max_cards? @player
-        rescue RuntimeError => e
-          puts e.message
-          user_actions
-        end
-        @player.take_card @deck
-        winner if @player.score >= GAME_GOAL
-        dealer_actions
-      else
-        puts "Неизвестная команда"
+    when 1
+      dealer_actions
+    when 2
+      winner
+    when 3
+      begin
+        raise 'Взято максимальное количество карт' if max_cards? @player
+      rescue RuntimeError => e
+        puts e.message
         user_actions
       end
+      @player.take_card @deck
+      winner if @player.score >= GAME_GOAL
+      dealer_actions
+    else
+      puts 'Неизвестная команда'
+      user_actions
+    end
   end
 
-  def max_cards? user
+  def max_cards?(user)
     user.cards.length >= MAX_CARDS
   end
 end
